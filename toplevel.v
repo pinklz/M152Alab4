c4dd01d7e3fdb2d4d04979a5ab52047a049100eb
Elizabeth Manka <116235230+pinklz@users.noreply.github.com>
2024-05-28 11:48:10 -0700

    Update toplevel.v

--- toplevel.v
+++ toplevel.v
@@ -1,26 +1,34 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 28.05.2024 10:42:44
-// Design Name: 
-// Module Name: toplevel
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module toplevel(
-
+    input wire clk,			//master clock = 100MHz
+    input wire rst,            //right-most pushbutton for reset
+    output wire [2:0] red,    //red vga output - 3 bits
+    output wire [2:0] green,//green vga output - 3 bits
+    output wire [2:0] blue,    //blue vga output - 3 bits
+    output wire hsync,        //horizontal sync out
+    output wire vsync            //vertical sync out
     );
+
+
+// VGA display clock interconnect
+wire dclk;
+
+clock_divider U1(
+	.clk(clk),
+	.rst(rst),
+	.dclk(dclk)
+	);
+
+// VGA controller
+display U3(
+	.dclk(dclk),
+	.rst(rst),
+	.hsync(hsync),
+	.vsync(vsync),
+	.red(red),
+	.green(green),
+	.blue(blue)
+	);
+	
 endmodule
