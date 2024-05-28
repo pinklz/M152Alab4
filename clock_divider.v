d5f2024177e1ab441718c3354ecd24451002447b
Elizabeth Manka <116235230+pinklz@users.noreply.github.com>
2024-05-28 11:49:01 -0700

    Update clock_divider.v

--- clock_divider.v
+++ clock_divider.v
@@ -1,6 +1,28 @@
 `timescale 1ns / 1ps
 
 module clock_divider(
+	input wire clk,		//master clock: 100MHz
+	input wire rst,		//asynchronous reset
+	output wire dclk		//pixel clock: 25MHz
+	);
 
-    );
+// 17-bit counter variable
+reg [17:0] q;
+
+// Clock divider --
+// Each bit in q is a clock signal that is
+// only a fraction of the master clock.
+always @(posedge clk or posedge rst)
+begin
+	// reset condition
+	if (rst == 1)
+		q <= 0;
+	// increment counter by one
+	else
+		q <= q + 1;
+end
+
+// 100Mhz ÷ 4 = 25MHz --bottom 2 bits will count from 0 to 4
+assign dclk = q[0] & q[1];
+
 endmodule
