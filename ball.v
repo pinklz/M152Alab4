b9d2d6997f5cff9fa7cf9229339a4e354b4e7e49
j-downing <129227445+j-downing@users.noreply.github.com>
2024-05-30 10:18:45 -0700

    Added readme describing module basic functionality

--- ball.v
+++ ball.v
@@ -1,6 +1,15 @@
 `timescale 1ns / 1ps
 
 module ball(
+    input wire clk,
+    input wire x_initial,
+    input wire y_initial,
+    input wire vx,
+    input wire vy,
 
+    output wire x_out,
+    output wire y_out,
     );
+
+
 endmodule
