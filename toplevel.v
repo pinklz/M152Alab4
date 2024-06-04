7615fb812b643b17458bacf746587f78a821591b
Elizabeth Manka <116235230+pinklz@users.noreply.github.com>
2024-06-04 11:50:21 -0700

    Update toplevel.v w/ ball and brick module declarations

--- toplevel.v
+++ toplevel.v
@@ -10,20 +10,23 @@ module toplevel(
     output wire [2:0] green,  // green vga output - 3 bits
     output wire [2:0] blue,   // blue vga output - 3 bits
     output wire hsync,        // horizontal sync out
-    output wire vsync         // vertical sync out
+    output wire vsync,         // vertical sync out
+    
+    output reg [6:0] seven_seg_display,
+    output reg [3:0] an
     );
     
-    wire [6:0] sevenmin1;
-    wire [6:0] sevenmin0;
-    wire [6:0] sevensec0;
-    wire [6:0] sevensec1;
+    wire [6:0] thous;
+    wire [6:0] huns;
+    wire [6:0] tens;
+    wire [6:0] ones;
     wire [6:0] no_val;
     
-    wire [3:0] sec0cnt;
-    wire [3:0] sec1cnt;
-    wire [3:0] min0cnt;
-    wire [3:0] min1cnt;
-    wire [3:0] ones = 4'b1111;
+    wire [3:0] onescnt;
+    wire [3:0] tenscnt;
+    wire [3:0] hunscnt;
+    wire [3:0] thouscnt;
+    wire [3:0] novalue = 4'b1111;
     
     // TODO: use score module to set sec0, sec1, min0, min1cnt 's
     
@@ -41,6 +44,21 @@ module toplevel(
         .button_out(reset)
     );
     
+    wire left;
+    wire right;
+    
+    debouncer move_left(
+        .button_in(l),
+        .clk(clk),
+        .button_out(left)
+    );
+    
+    debouncer move_right(
+            .button_in(r),
+            .clk(clk),
+            .button_out(right)
+);
+    
 
 // VGA display clock interconnect
 wire dclk;
@@ -54,69 +72,83 @@ clock_divider U1(
     );
     
     
-seven min1(
-       .dig(min1cnt),
-       .seven_seg_display(sevenmin1)
+/******* SCORE COUNTING ********/
+wire hit = 0;
+score score(
+    .clk(clk),
+    .reset(rst),
+    .collision(hit),
+    .thous(thouscnt),
+    .huns(hunscnt),
+    .tens(tenscnt),
+    .ones(onescnt)
+);
+    
+seven thousand(
+       .dig(thouscnt),
+       .seven_seg_display(thous)
 );
            
-seven min0(
-       .dig(min0cnt),
-       .seven_seg_display(sevenmin0)
+seven hundred(
+       .dig(hunscnt),
+       .seven_seg_display(huns)
 );
 
-seven sec1(
-       .dig(sec1cnt),
-       .seven_seg_display(sevensec1)
+seven ten(
+       .dig(tenscnt),
+       .seven_seg_display(tens)
 );
 
-seven sec0(
-       .dig(sec0cnt),
-       .seven_seg_display(sevensec0)
+seven one(
+       .dig(onescnt),
+       .seven_seg_display(ones)
 );
     
     
 seven no_value(
-      .dig(ones),
+      .dig(novalue),
       .seven_seg_display(no_val)
 );
 
-wire left;
-wire right;
-
-// Set LEFT and RIGHT based on button input
-debouncer move_left(
-    .button_in(l),
-    .clk(clk),
-    .button_out(left)
-    );
-    
-debouncer move_right(
-        .button_in(r),
-        .clk(clk),
-        .button_out(right)
-    );
-
 // Board position parameters to center the board
-wire board_x = 320 - 32; // 640/2 - 64/2
-wire board_y = 300 - 4;  // 480/2 - 8/2
+wire [9:0] board_x_init = 320 - 32; // 640/2 - 64/2
+wire [9:0] board_y = 300 - 4;  // 480/2 - 8/2
+
+wire [9:0] board_x;
 
 board paddle(
     .clk(clk),
     .reset(reset),
     .move_left(left),
     .move_right(right),
-    .x_initial(board_x),    // TODO: figure out how to do input x and output x
-    .y_intiial(board_y), 
+    .x_initial(board_x_init),    // TODO: figure out how to do input x and output x
+    .y_initial(board_y) 
 );
-    
 
+wire [9:0] ball_x;
+wire [9:0] ball_y;
 
+ball ball(
+    .clk(clk),
+    .reset(rst),
+    .pause(paus),
+    .x_initial(board_x_init)  
+);
+
+wire [9:0] brick1_x = 320 - 32;
+wire [9:0] brick1_y = 300;
+bricks brick(
+    .clk(clk),
+    .reset(rst),
+    .x_pos(brick1_x),
+    .y_pos(brick1_y)
+);
 
 // VGA controller
 display U3(
     .dclk(dclk),
     .rst(rst),
-    .board_x(board_x),
+    .board_x(board_x_init),
     .board_y(board_y),
     .hsync(hsync),
     .vsync(vsync),
@@ -124,5 +156,32 @@ display U3(
     .green(green),
     .blue(blue)
     );
+    
+    
+/***** DISPLAY SCORE ******/
+reg [1:0] which_digit = 2'b00;
+
+always @(posedge seghz) begin
+    if (which_digit == 0) begin
+        which_digit <= 1;
+        an <= 4'b0111;
+        seven_seg_display <= thous;
+    end
+    if (which_digit == 1) begin
+        which_digit <=2;
+        an <= 4'b1011;
+        seven_seg_display <= huns;
+    end
+    if (which_digit == 2) begin
+        which_digit <= 3;
+        an <= 4'b1101;
+        seven_seg_display <= tens;
+    end
+    if (which_digit == 3) begin
+        which_digit <= 0;
+        an <= 4'b1110;
+        seven_seg_display <= ones;
+    end
+end
     
 endmodule
