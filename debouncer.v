b0f0d2bb2a9625a16b966f36de304f7d5a906077
Elizabeth Manka <116235230+pinklz@users.noreply.github.com>
2024-06-05 17:16:59 -0700

    Try new type of debouncer

--- debouncer.v
+++ debouncer.v
@@ -11,19 +11,33 @@ module debouncer(
 reg [3:0] mask;      // counter register
 reg button = 0;
 
+localparam num_flops = 4;
+reg [3:0] shift_reg;
+
 always @(posedge clk) begin
-    if (button_in == 0) begin
-        mask <= 0;
-        button <= 0;
+    shift_reg <= {shift_reg[2:0], button_in};
+    
+    if (shift_reg == {4{1'b1}}) begin
+        button <= 1;
     end
     else begin
-        mask <= mask + 1;
-        if (mask == 4'b1111) begin
-            button <= 1;
-            mask <= 0;
-        end
+        button <=0;
     end
 end
+
+//always @(posedge clk) begin
+//    if (button_in == 0) begin
+//        mask <= 0;
+//        button <= 0;
+//    end
+//    else begin
+//        mask <= mask + 1;
+//        if (mask == 4'b1111) begin
+//            button <= 1;
+//            mask <= 0;
+//        end
+//    end
+//end
 
 assign button_out = button;
 endmodule
